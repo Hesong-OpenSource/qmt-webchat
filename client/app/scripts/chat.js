743f1c72013354f86b4d40b2fb487b215b65e44f
lys <lys@hesong.net>
2014-09-04 09:45:23 +0800

    添加图片上传，限制图片大小及格式，限制聊天发消息时间间隔，修正表情显示问题

--- client/app/scripts/chat.js
+++ client/app/scripts/chat.js
@@ -1,23 +1,70 @@
 /**
  * Created by user on 2014/8/30.
  */
-var socket = null;
+
 $(document).ready(function () {
-    var emoji_pic_path='emoji/qqemoji/';
+    if(typeof console=="undefined"){
+
+        console = {
+            log: function () {
+            },
+            error: function () {
+            }
+        };
+    }
+    if (!$.support.leadingWhitespace) {
+
+        var d = $("#divcontent");
+        d.removeClass("divcontent");
+        d.addClass("divcontent_ie8");
+    }
+    messages = [];
+    var socket = null;
+    var divchat = $("#divcontent")[0];
+    var send_msg_box = $('#msgTxt');
+    var emoji_pic_path = 'emoji/qqemoji/';
+
     $('.emotion').qqFace({
-        id : 'facebox',
-        assign:'msgTxt',
-        path:emoji_pic_path	//表情存放的路径
+        id: 'facebox',
+        assign: 'msgTxt',
+        path: emoji_pic_path	//表情存放的路径
     });
 
     $("body").keydown(function (event) {
         if (event.ctrlKey && event.keyCode == '13') {
             sendmsg();
+
         }
     });
-    var divchat = $("#divcontent")[0];
-    var send_msg_box = $('#msgTxt');
-    $("#b_captcha").click(function () {
+    function authed(){
+        var result;
+        result = $.ajax({
+            url: "/authed",
+            type: "get",
+            cache: false,
+            dataType: "json",
+            contentType: 'application/json; charset=UTF-8',
+            timeout: 60000,
+            success: function (data, status, xhr) {
+                if (data.authed) {
+                    $("#d_captcha")[0].style.display = "none";
+                    init_sockio();
+                    tip("请输入您要咨询的问题，按ctrl+enter发送");
+                } else {
+                    $("#d_captcha")[0].style.display = "";
+                    tip("请通过验证后再输入您要咨询的内容，按ctrl+enter发送");
+                    $("#t_captcha")[0].focus();
+
+                }
+            },
+            error: function (xhr, status, ex) {
+                console.log(ex);
+
+            }
+        });
+    }
+    authed();
+    $("#form_captcha").submit(function (event) {
         var result;
         result = $.ajax({
             url: "/captcha",
@@ -30,22 +77,7 @@ $(document).ready(function () {
             success: function (data, status, xhr) {
                 if (data.result) {
                     $("#d_captcha")[0].style.display = "none";
-                    socket = io();
-                    /**
-                     接收消息
-                     */
-                    socket.on('ImMessage', function (data) {
-                        console.log(data);
-                        var content = parse_content(data.Content);
-                        console.log(content);
-                        messages.push({
-                            side: 'left',
-                            text: content,
-                            avatar: 'http://img0.bdstatic.com/img/image/shouye/mxlss33-11681559436.jpg'
-                        });
-
-                        divchat.scrollTop = divchat.scrollHeight;
-                    });
+                    init_sockio();
                     tip("验证成功,请输入您要咨询的问题，按ctrl+enter发送");
                 } else {
                     tip("验证码错误，请重新输入");
@@ -58,11 +90,32 @@ $(document).ready(function () {
 
             }
         });
-
-
+//    event.preventDefault();
+        return false;
     });
 
-    messages = [];
+    function init_sockio(){
+        socket = io();
+        /**
+         接收消息
+         */
+        socket.on('ImMessage', function (data) {
+            console.log(data);
+            var content = parse_content(data.Content);
+            console.log(content);
+            var regRN = /\r\n/g;
+            content = content.replace(regRN, "<br />");
+            var regR = /\r/g;
+            var regN = /\n/g;
+            content = content.replace(regR, "<br />").replace(regN, "<br />");
+            content = reg_email(content);
+            displayMsg({
+                side: 'left',
+                text: content,
+                avatar: 'http://img0.bdstatic.com/img/image/shouye/mxlss33-11681559436.jpg'
+            });
+        });
+    }
 
     ractive = new Ractive({
         el: 'messages_output',
@@ -78,6 +131,7 @@ $(document).ready(function () {
 /// 发送消息
     $('#msgFrm').submit(function (event) {
         sendmsg();
+        //    event.preventDefault();
         return false;
     });
     var sendmsg = function () {
@@ -88,16 +142,19 @@ $(document).ready(function () {
         }
         var msg = send_msg_box.val();
         send_msg_box.val('');
+        msg = msg.trim();
         if (msg == "") {
             tip("不能发送空消息");
 
             return false;
         }
+
         var dt = new Date();
-        messages.push({
-            avatar: 'http://pic.baike.soso.com/p/20130309/bki-20130309223642-2077810791.jpg',
+
+        displayMsg({
             side: 'right',
-            text: replace_em(msg)
+            text: reg_email(reg_http( parse_content(msg))),
+            avatar: 'http://pic.baike.soso.com/p/20130309/bki-20130309223642-2077810791.jpg'
         });
         var data = {
             FromUserName: '光头强',
@@ -115,7 +172,15 @@ $(document).ready(function () {
             contentType: 'application/json; charset=UTF-8',
             timeout: 60000,
             success: function (data, status, xhr) {
-                console.log(data);
+                if(data){
+                    if(data.desc){
+                        displayMsg({
+                            side: 'left',
+                            text: data.desc,
+                            avatar: 'http://img0.bdstatic.com/img/image/shouye/mxlss33-11681559436.jpg'
+                        });
+                    }
+                }
             },
             error: function (xhr, status, ex) {
                 console.log(ex);
@@ -126,14 +191,36 @@ $(document).ready(function () {
         tip("请输入您要咨询的问题，按ctrl+enter发送");
 
 //        socket.emit('ImMessage', data);
-        event.preventDefault();
+
     };
-    function replace_em(str){
-        str = str.replace(/\</g,'&lt;');
-        str = str.replace(/\>/g,'&gt;');
-        str = str.replace(/\n/g,'<br/>');
-        str = str.replace(/\[em_([0-9]*)\]/g,'<img src="./'+emoji_pic_path+'$1.png" border="0" />');
+
+//    function replace_em(str) {
+//        str = str.replace(/\</g, '&lt;');
+//        str = str.replace(/\>/g, '&gt;');
+//        str = str.replace(/\n/g, '<br/>');
+//        str = str.replace(/\[em_([0-9]*)\]/g, '<img src="./' + emoji_pic_path + '$1.png" border="0" />');
+//        return str;
+//    }
+
+    function reg_email(str){
+        str = str.replace(/\b(\w+@[\w+\.?]*)/gi,
+            "<a href=\"mailto\:$1\" target='_blank '>$1</a>");
         return str;
+    }
+
+    function reg_http(str) {
+
+        str = str.replace(/\bhttp\:\/\/www(\.[\w+\.\:\/\_]+)/gi,
+            "http\:\/\/&not;¤&cedil;$1");
+        str = str.replace(/\b(http\:\/\/\w+\.[\w+\.\:\/\_]+)/gi,
+            "<a href=\"$1\" target='_blank '>$1<\/a>");
+        str = str.replace(/\b(www\.[\w+\.\:\/\_]+)/gi,
+            "<a href=\"http://$1\" target='_blank '>$1</a>");
+        str = str.replace(/\bhttp\:\/\/&not;¤&cedil;(\.[\w+\.\:\/\_]+)/gi,
+            "<a href=\"http\:\/\/www$1\" target='_blank '>http\:\/\/www$1</a>");
+
+        return str;
+
     }
 
     $('#btn_rg').click(function (event) {
@@ -151,7 +238,15 @@ $(document).ready(function () {
             contentType: 'application/json; charset=UTF-8',
             timeout: 60000,
             success: function (data, status, xhr) {
-                console.log(data);
+                if(data){
+                    if(data.desc){
+                        displayMsg({
+                            side: 'left',
+                            text: data.desc,
+                            avatar: 'http://img0.bdstatic.com/img/image/shouye/mxlss33-11681559436.jpg'
+                        });
+                    }
+                }
             },
             error: function (xhr, status, ex) {
                 console.log(ex);
@@ -161,7 +256,37 @@ $(document).ready(function () {
         // socket.emit('ImMessage', data);
     });
 
+    $("#open_upload_img").click(function(event){
+        $("#upload_img").click();
+    });
 
+//    $("#upload_img")
+    upclick(
+        {
+            element: "upload_img",
+            action: '/uploadfile/',
+            action_params : {},
+            onstart:
+                function(filename)
+                {
+                    console.log("filename is ",filename);
+                },
+            oncomplete:
+                function(response_data)
+                {
+                    var data=response_data=="null"?"上传成功":response_data;
+                    displayMsg({
+                        side: 'left',
+                        text: data,
+                        avatar: 'http://img0.bdstatic.com/img/image/shouye/mxlss33-11681559436.jpg'
+                    });
+
+                }
+        });
+    function displayMsg(msg){
+        messages.push(msg);
+        divchat.scrollTop = divchat.scrollHeight;
+    }
 });
 
 
--- server/main.js
+++ server/main.js
@@ -20,17 +20,73 @@ var sessionStore = new RedisStore({
 });
 app.use(session({
     store: sessionStore,
-    secret:cookieSecret
+    secret: cookieSecret
 }));
-/*var cookie_session = require('cookie-session');
- app.use(cookie_session({
- secret:cookieSecret,
- keys: ['sid']
- }));*/
-
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var captchapng = require('captchapng');
+var ftpclientobj = require('ftp');
+var multer = require('multer');
+var ftpimgpath="/webchat_upload_imgs/";
+var allow_img=["jpg","jpeg","png","gif","bmp"];
+var fs=require("fs");
+app.use(multer({
+    dest: './uploadimgs/',
+    limits: {
+        fieldNameSize: 100,
+        fileSize:1024*200,
+        files: 1
+    },
+    onFileUploadStart: function (file) {
+        if (!file.extension.toLowerCase() in allow_img) return false;
+
+    },
+    onFileUploadData: function (file, data) {
+
+    },
+    onFileUploadComplete: function (file) {
+        var ftpclient =  new ftpclientobj();
+        ftpclient.on('ready', function () {
+            ftpclient.put(file.path, ftpimgpath+file.name,false,function(err){
+                fs.exists(file.path,function(isexist){
+                    if(isexist){
+                        fs.unlink(file.path,function(err){
+                            if(err){
+                                console.log("delete upload image error:",err);
+                            }
+                        });
+                    }
+
+                });
+            });
+        });
+        ftpclient.on('error', function(){
+            // TODO: return 5xx
+        });
+        ftpclient.connect({
+            host: "10.4.62.41",
+            port: 21,
+            user: "Administrator",
+            password: "Ky6241",
+            pasvTimeout: 2000,
+            keepalive: 2000
+        });
+    },
+    onParseStart: function () {
+        console.log('Form parsing started at: ', new Date())
+    },
+    onFileSizeLimit:function(file){
+        fs.exists(file.path,function(isexist){
+            if(isexist){
+                fs.unlink(file.path,function(err){
+                    if(err){
+                        console.log("delete upload image error:",err);
+                    }
+                });
+            }
+        });
+    }
+}))
 
 /**
  * 全局变量
@@ -65,15 +121,17 @@ app.post('/qmtapi/staffService/message', function (req, res) {
     /// 转发
     var data = req.body;
     var sid = data.ToUserId;
-    sessionStore.load(sid, function(err, session){
+
+    sessionStore.load(sid, function (err, session) {
         if (err || !session) {
             res.status(500).send({
                 errcode: 0,
                 errmsg: err
             });
 
-        }else{
-            console.log("send to io ",session.ioid);
+        } else {
+            console.log("send to io ", session.ioid);
+            console.log("send to socked client message:", data);
             io.to(session.ioid).emit('ImMessage', data);
             res.status(200).send({
                 errcode: 0,
@@ -86,6 +144,43 @@ app.post('/qmtapi/staffService/message', function (req, res) {
 });
 
 
+function sendmsg_to_qmt(data, req, res) {
+    req.session.touch();
+    var sha1 = crypto.createHash('sha1');
+    sha1.update(appid);
+    sha1.update(appsecret);
+    var d = new Date();
+    var timestamp = d.getTime().toString();
+    sha1.update(timestamp);
+    var signature = sha1.digest('hex');
+    /// POST 到全媒体 IM WEBAPI
+    // 用 socketio 的 ID 当作 FromUserIDv
+    // var data = req.body;
+    data.FromUserId = req.sessionID;
+    var qmturl = util.format('http://10.4.62.41:8080/weChatAdapter/api/v1/%s/staffService/message?timestamp=%s&signature=%s',
+        appid, timestamp, signature);
+    console.log("send to qmt message:", data);
+    console.log("send to qmt url:", qmturl)
+    request.post({
+            uri: qmturl,
+            body: data,
+            json: true
+        },
+        function (error, response, body) {
+            if (error) {
+                res.status(500).send(error);
+                return console.error(error, body);
+            } else {
+                if (body.errcode != 0) {
+                    res.status(500).send(body);
+                    return console.error(body);
+                }
+            }
+            console.log(body);
+            res.status(200).send(JSON.stringify(null));
+        }
+    );
+}
 io.use(function (socket, next) {
     if (socket.request.headers.cookie) return next();
     next(new Error('Authentication error'));
@@ -95,27 +190,7 @@ io.on('connection', function (socket) {
     socket.on('disconnect', function () {
 
     });
-    try{
-        var signedCookies = require('express/node_modules/cookie').parse(socket.request.headers.cookie)['connect.sid'];
-        var sid = signedCookies.split(':')[1].split('.')[0];
-        sessionStore.load(sid, function (err, session) {
-            if (err || !session) {
-                socket.close();
-
-            }else{
-                session.ioid = socket.id;
-                sessionStore.set(sid, session);
-            }
-        });
-    }catch (ex){
-        console.error(ex);
-        socket.close();
-    }
-
-
-    //接收浏览器的消息，并发送到qmt im中。
-  /*  socket.on('ImMessage', function (data) {
-        console.log(socket.id + ': Send to QMT: ' + data);
+    try {
         var signedCookies = require('express/node_modules/cookie').parse(socket.request.headers.cookie)['connect.sid'];
         var sid = signedCookies.split(':')[1].split('.')[0];
         sessionStore.load(sid, function (err, session) {
@@ -123,107 +198,186 @@ io.on('connection', function (socket) {
                 socket.close();
 
             } else {
-
-                if(session.authed){
-                    // 全媒体 IM WEBAPI 的签名
-                    var sha1 = crypto.createHash('sha1');
-                    sha1.update(appid);
-                    sha1.update(appsecret);
-                    var d = new Date();
-                    var timestamp = d.getTime().toString();
-                    sha1.update(timestamp);
-                    var signature = sha1.digest('hex');
-                    /// POST 到全媒体 IM WEBAPI
-                    // 用 socketio 的 ID 当作 FromUserID
-                    data.FromUserId = socket.id;
-                    request.post({
-                            uri: util.format('http://10.4.62.41:8080/weChatAdapter/api/v1/%s/staffService/message?timestamp=%s&signature=%s',
-                                appid, timestamp, signature),
-                            body: data,
-                            json: true
-                        },
-                        function (error, response, body) {
-                            if (error) {
-                                return console.error(error, body);
-                            } else {
-                                if (body.errcode != 0) {
-                                    console.error(body);
-                                }
-                            }
-                            console.log(body);
-                        }
-                    );
-                }else{
-                    socket.close();
-                }
-
+                session.ioid = socket.id;
+                sessionStore.set(sid, session);
             }
         });
+    } catch (ex) {
+        console.error(ex);
+        socket.close();
+    }
 
 
-        // for (var i in io_sessions) {
-        //     var sock_id = io_sessions[i];
-        //     console.log(sock_id);
-        //     io.to(sock_id).emit('chat message', socket.id + ': ' + msg)
-        // }
-    });*/
+    //接收浏览器的消息，并发送到qmt im中。
+    /*  socket.on('ImMessage', function (data) {
+     console.log(socket.id + ': Send to QMT: ' + data);
+     var signedCookies = require('express/node_modules/cookie').parse(socket.request.headers.cookie)['connect.sid'];
+     var sid = signedCookies.split(':')[1].split('.')[0];
+     sessionStore.load(sid, function (err, session) {
+     if (err || !session) {
+     socket.close();
+
+     } else {
+
+     if(session.authed){
+     // 全媒体 IM WEBAPI 的签名
+     var sha1 = crypto.createHash('sha1');
+     sha1.update(appid);
+     sha1.update(appsecret);
+     var d = new Date();
+     var timestamp = d.getTime().toString();
+     sha1.update(timestamp);
+     var signature = sha1.digest('hex');
+     /// POST 到全媒体 IM WEBAPI
+     // 用 socketio 的 ID 当作 FromUserID
+     data.FromUserId = socket.id;
+     request.post({
+     uri: util.format('http://10.4.62.41:8080/weChatAdapter/api/v1/%s/staffService/message?timestamp=%s&signature=%s',
+     appid, timestamp, signature),
+     body: data,
+     json: true
+     },
+     function (error, response, body) {
+     if (error) {
+     return console.error(error, body);
+     } else {
+     if (body.errcode != 0) {
+     console.error(body);
+     }
+     }
+     console.log(body);
+     }
+     );
+     }else{
+     socket.close();
+     }
+
+     }
+     });
+
+
+     // for (var i in io_sessions) {
+     //     var sock_id = io_sessions[i];
+     //     console.log(sock_id);
+     //     io.to(sock_id).emit('chat message', socket.id + ': ' + msg)
+     // }
+     });*/
 });
 /*
-var makeUUID = function () {
-    var S4 = function () {
-        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-    };
-    return (S4() + S4() + S4() + S4() + S4() + S4() + S4() + S4()).toUpperCase();
-}
-*/
-app.post("/sendmsg",function(req,res,next){
-    if(req.session.authed){
-        req.session.touch();
-        var sha1 = crypto.createHash('sha1');
-        sha1.update(appid);
-        sha1.update(appsecret);
-        var d = new Date();
-        var timestamp = d.getTime().toString();
-        sha1.update(timestamp);
-        var signature = sha1.digest('hex');
-        /// POST 到全媒体 IM WEBAPI
-        // 用 socketio 的 ID 当作 FromUserIDv
-        var data=req.body;
-        data.FromUserId = req.sessionID;
-        request.post({
-                uri: util.format('http://10.4.62.41:8080/weChatAdapter/api/v1/%s/staffService/message?timestamp=%s&signature=%s',
-                    appid, timestamp, signature),
-                body: data,
-                json: true
-            },
-            function (error, response, body) {
-                if (error) {
-                    res.status(500).send(error);
-                    return console.error(error, body);
-                } else {
-                    if (body.errcode != 0) {
-                        res.status(500).send(body);
-                        return console.error(body);
-                    }
-                }
-                console.log(body);
-                res.status(200).send();
-            }
-        );
+ var makeUUID = function () {
+ var S4 = function () {
+ return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+ };
+ return (S4() + S4() + S4() + S4() + S4() + S4() + S4() + S4()).toUpperCase();
+ }
+ */
+app.post("/uploadfile", function (req, res, next) {
+    var imgurl =ftpimgpath+ req.files.Filedata.name;
+    var filesize=req.files.Filedata.size;
+    if(filesize>=204800){
+         res.status(400).send("图片文件不能超过200k");
     }else{
+        var allowpic=false;
+        for(var i=0;i<allow_img.length;i++){
+            if(allow_img[i]==req.files.Filedata.extension.toLowerCase()){
+                allowpic=true;
+                sendmsg_to_qmt({Content: imgurl, FromUserName: "光头强", MsgType: "image"}, req, res);
+                break;
+            }
+        }
+        if(!allowpic){
+            var str="图片格式必须为:";
+            for(var i=0;i<allow_img.length;i++){
+                if(i<(allow_img.length-1)){
+                    str=str+allow_img[i]+",";
+                }else{
+                    str=str+allow_img[i];
+                }
+
+            }
+            res.status(400).send(str);
+        }
+    }
+
+
+
+});
+app.post("/sendmsg", function (req, res, next) {
+    if (req.session.authed) {
+        if(req.body.Content){
+            if(req.body.Content.length>500){
+                res.status(200).send(JSON.stringify({desc:"一次发送内容不能超过500个文字！"}));
+                return;
+            }
+        }
+
+        var cTime=new Date();
+        if( req.session.last_msg_time){
+            var timelen=cTime.getTime()-Date.parse(req.session.last_msg_time);
+            if(timelen<1000){
+                //TODO:提示客户端间隔
+                res.status(200).send(JSON.stringify({desc:"亲，别急，喝口茶先！"}));
+                return;
+            }
+        }
+        req.session.last_msg_time=cTime;
+        sendmsg_to_qmt(req.body, req, res);
+        /*req.session.touch();
+         var sha1 = crypto.createHash('sha1');
+         sha1.update(appid);
+         sha1.update(appsecret);
+         var d = new Date();
+         var timestamp = d.getTime().toString();
+         sha1.update(timestamp);
+         var signature = sha1.digest('hex');
+         /// POST 到全媒体 IM WEBAPI
+         // 用 socketio 的 ID 当作 FromUserIDv
+         var data = req.body;
+         data.FromUserId = req.sessionID;
+         var qmturl = util.format('http://10.4.62.41:8080/weChatAdapter/api/v1/%s/staffService/message?timestamp=%s&signature=%s',
+         appid, timestamp, signature);
+         console.log("send to qmt message:", data);
+         console.log("send to qmt url:", qmturl)
+         request.post({
+         uri: qmturl,
+         body: data,
+         json: true
+         },
+         function (error, response, body) {
+         if (error) {
+         res.status(500).send(error);
+         return console.error(error, body);
+         } else {
+         if (body.errcode != 0) {
+         res.status(500).send(body);
+         return console.error(body);
+         }
+         }
+         console.log(body);
+         res.status(200).send(JSON.stringify(null));
+         }
+         );*/
+    } else {
         res.status(403).send();
     }
 });
-
+app.get("/authed", function (req, res, next) {
+    if (req.session.authed) {
+        res.status(200).send(JSON.stringify({authed: true}));
+    } else {
+        res.status(200).send(JSON.stringify({authed: false}));
+    }
+});
 app.route('/captcha').get(function (req, res, next) {
+
     if (req.session.captcha_val) {
-        console.log('pre-val in cookie is ', req.session.captcha_val);
+        console.log('pre-val in session is ', req.session.captcha_val);
     }
     if (req.session.tag) {
         console.log("session tag is ", req.session.tag);
     }
     var captchaValue = parseInt(Math.random() * 9000 + 1000);
-    var p = new captchapng(60, 25, captchaValue); // width,height,numeric captcha
+    var p = new captchapng(50, 18, captchaValue); // width,height,numeric captcha
 
     p.color(0, 0, 0, 0);  // First color: background (red, green, blue, alpha)
     p.color(80, 80, 80, 255); // Second color: paint (red, green, blue, alpha)
@@ -235,18 +389,18 @@ app.route('/captcha').get(function (req, res, next) {
     res.set('Content-Type', 'image/png');
     res.status(200).send(imgbase64);
 }).post(function (req, res, next) {
-    var data = req
-    console.log("submit captcha is ", data.body.captcha);
-    if (data.body.captcha == req.session.captcha_val) {
-        delete req.session.captcha_val;
-        //var guid=makeUUID();
-        req.session.authed = true;
-        res.status(200).send({result: true});
-    } else {
-        res.status(200).send({result: false});
-    }
+        var data = req
+        console.log("submit captcha is ", data.body.captcha);
+        if (data.body.captcha == req.session.captcha_val) {
+            delete req.session.captcha_val;
+            //var guid=makeUUID();
+            req.session.authed = true;
+            res.status(200).send({result: true});
+        } else {
+            res.status(200).send({result: false});
+        }
 
-});
+    });
 http.listen(3000, function () {
     console.log('listening on * : 3000');
 });
